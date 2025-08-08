84063324a26681a2b36d817a6f16dc8faeee2ad3
Kritika786 <73874564+Kritika786@users.noreply.github.com>
2025-08-08 16:58:10 +0530

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,13 +1,5 @@
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                steps {
-                    echo "Building the app..."
-                    sh './gradlew build' // or mvn clean install
-                }
-            }
-        }
-    }
+    echo "Building the app..."
+    sh './gradlew build'  // or mvn clean install
 }
+
