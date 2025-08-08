e9dcaa98ddfaf0f9b22a1e5aaaa313de98dffeac
Kritika786 <73874564+Kritika786@users.noreply.github.com>
2025-08-08 16:59:51 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,13 +1,5 @@
 def call(String env = "dev") {
-    pipeline {
-        agent any
-        stages {
-            stage('Deploy') {
-                steps {
-                    echo "Deploying to ${env}..."
-                    sh "kubectl apply -f k8s/${env}/deployment.yaml"
-                }
-            }
-        }
-    }
+    echo "Deploying to ${env}..."
+    sh "kubectl apply -f k8s/${env}/deployment.yaml"
 }
+
